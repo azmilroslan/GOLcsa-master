e7586e280a9a161f9994dc0a91cfbf90783e4532
azmilroslan <azmilroslan01@gmail.com>
2021-11-11 22:44:37 +0000

    1st commit

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,37 +1,59 @@
 package gol
 
+import (
+	"strconv"
+	"strings"
+)
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
 	ioIdle     <-chan bool
 	ioFilename chan<- string
-	ioOutput   chan<- uint8
 	ioInput    <-chan uint8
+}
+
+// func to create an empty 2D slice (world)
+func createSlice(p Params, height int) [][]byte {
+	newSlice := make([][]byte, height)
+	for i := 0; i < height; i++ {
+		newSlice[i] = make([]byte, p.ImageWidth)
+	}
+	return newSlice
 }
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
-	world := make([][]byte, p.ImageHeight)
-	for i := 0; i < p.ImageHeight; i++ {
-		world[i] = make([]byte, p.ImageWidth)
-	}
-	
-	turn := 0
+	world := createSlice(p, p.ImageHeight)
 
+	//request to read in pgm file
+	c.ioCommand <- ioInput
+	//read in the concatenated ImageWidth and ImageHeight and pass it to the channel
+	c.ioFilename <- strings.Join([]string{strconv.Itoa(p.ImageWidth), strconv.Itoa(p.ImageHeight)}, "x")
+
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			val := <-c.ioInput
+			if val != 0 {
+				world[y][x] = val
+			}
+		}
+	}
+
+	turn := 0
 
 	// TODO: Execute all turns of the Game of Life.
 
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
 	c.events <- StateChange{turn, Quitting}
-	
+
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
 }
