8d37e1c89432ed7226244ebda3912a4bcc1915bf
carla-rosario <78482746+carla-rosario@users.noreply.github.com>
2021-11-21 18:10:16 +0000

    Tidied powofTwo section

--- gol/distributor.go
+++ gol/distributor.go
@@ -19,10 +19,13 @@ type distributorChannels struct {
 
 //GOL Logic
 
-func worker(p Params, world, emptyWorld [][]byte, thread, workerHeight, extraPixel int, powOfTwo bool, waitGroup *sync.WaitGroup) {
+func worker(p Params, world, emptyWorld [][]byte, thread, workerHeight int, waitGroup *sync.WaitGroup) {
 	yBound := (thread + 1) * workerHeight
 
-	if powOfTwo { //if not splitted perfectly, add 'extra' pixel
+	//if thread is not power of 2, "splitted" image will need "extra" pixels
+	extraPixel := p.ImageHeight % p.Threads
+
+	if !isPowOfTwo(p.Threads) {
 		yBound += extraPixel
 	}
 
@@ -87,6 +90,24 @@ func createSlice(p Params, height int) [][]byte {
 	return newSlice
 }
 
+//helper function to det. thread dimensions
+//Since the image HxW are power of two's, it can only be split
+//perfectly if the number of threads are power of two's
+//this part will check if p.Threads is a power of two
+func isPowOfTwo(n int) bool {
+	if n == 0 {
+		return false
+	} else {
+		for n != 0 {
+			if n%2 != 0 {
+				return false
+			}
+			n = n / 2
+		}
+		return true
+	}
+}
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
@@ -96,25 +117,6 @@ func distributor(p Params, c distributorChannels) {
 	updateWorld := createSlice(p, p.ImageHeight)
 	workerHeight := p.ImageHeight / p.Threads // 'split' the work (like in Median Filter lab)
 
-	//Since the image HxW are power of two's, it can only be splitted
-	//perfectly if the number of threads are power of two's
-	//this part will check if p.Threads is a power of two
-	var isPowOfTwo bool
-	n := p.Threads
-	if n == 0 {
-		isPowOfTwo = false
-	} else {
-		for n != 0 {
-			if n%2 != 0 {
-				isPowOfTwo = false
-			}
-			n = n / 2
-		}
-		isPowOfTwo = true
-	}
-
-	//if thread is not power of 2, "splitted" image will need "extra" pixels
-	extra := p.ImageHeight % p.Threads
 
 	//request to read in pgm file
 	c.ioCommand <- ioInput
@@ -153,11 +155,7 @@ func distributor(p Params, c distributorChannels) {
 			var wg = sync.WaitGroup{}        //used to make sure all goroutines have done executing before resuming
 			wg.Add(p.Threads)                //add number of threads the wait group needs to wait
 			for i := 0; i < p.Threads; i++ { //for each thread make the worker work??
-				if isPowOfTwo {
-					go worker(p, world, updateWorld, i, workerHeight, extra, true, &wg)
-				} else {
-					go worker(p, world, updateWorld, i, workerHeight, extra, false, &wg)
-				}
+				go worker(p, world, updateWorld, i, workerHeight, &wg)
 			}
 			wg.Wait() //wait till all goroutines is done (wg == 0)
 			turn = t + 1
