e949e899aa34324d9a303a56d9767fc646fab389
carla-rosario <78482746+carla-rosario@users.noreply.github.com>
2021-11-22 14:24:07 +0000

    minor tidies

--- gol/distributor.go
+++ gol/distributor.go
@@ -21,12 +21,14 @@ type distributorChannels struct {
 //GOL Logic
 
 
-func worker(p Params, c distributorChannels, world, emptyWorld [][]byte, thread, workerHeight, extraPixel int, turn int, waitGroup *sync.WaitGroup) {
+func worker(p Params, c distributorChannels, world, emptyWorld [][]byte, thread, workerHeight, turn int, waitGroup *sync.WaitGroup) {
 
 	yBound := (thread + 1) * workerHeight
+	extra := p.ImageHeight % p.Threads
+
 
 	if !isPowOfTwo(p.Threads) {
-		yBound += extraPixel
+		yBound += extra
 	}
 
 	for y := thread * workerHeight; y < yBound; y++ {
@@ -118,7 +120,6 @@ func createSlice(p Params, height int) [][]byte {
 //perfectly if the number of threads are power of two's
 //this part will check if p.Threads is a power of two
 func isPowOfTwo(n int) bool {
-
 	if n == 0 {
 		return false
 	} else {
@@ -141,12 +142,6 @@ func distributor(p Params, c distributorChannels, keyChan <-chan rune) {
 	world := createSlice(p, p.ImageHeight)
 	updateWorld := createSlice(p, p.ImageHeight)
 	workerHeight := p.ImageHeight / p.Threads // 'split' the work (like in Median Filter lab)
-	powOfTwo := isPowOfTwo(p.Threads)
-	extra := p.ImageHeight % p.Threads
-	//Since the image HxW are power of two's, it can only be splitted
-	//perfectly if the number of threads are power of two's
-	//this part will check if p.Threads is a power of two
-
 
 
 	//request to read in pgm file
@@ -161,8 +156,9 @@ func distributor(p Params, c distributorChannels, keyChan <-chan rune) {
 			world[y][x] = val
 		}
 	}
-	ticker := time.NewTicker(2 * time.Second) //create a new ticker
+
 	turn := 0
+	ticker := time.NewTicker(2 * time.Second) //create a new ticker
 	var aliveCells []util.Cell
 
 	// TODO: Execute all turns of the Game of Life.
@@ -171,6 +167,7 @@ func distributor(p Params, c distributorChannels, keyChan <-chan rune) {
 		for t := 0; t < p.Turns; t++ {
 			//fmt.Printf("turn2 = %d", turn )
 
+			//SDL logic
 			select {
 			case k := <-keyChan:
 				if k == 's' {
@@ -189,10 +186,10 @@ func distributor(p Params, c distributorChannels, keyChan <-chan rune) {
 					}
 				}
 
+			//AliveCell logic
 			case <-ticker.C: //this bit will update AliveCellCount every 2 seconds
 				alive := 0
 				alive += countAliveCells(p, world)
-				//fmt.Printf("turn3 = %d", turn)
 				if turn != 0 {
 					c.events <- AliveCellsCount{turn, alive}
 				} else {
@@ -200,15 +197,12 @@ func distributor(p Params, c distributorChannels, keyChan <-chan rune) {
 				}
 			default:
 			}
-			//fmt.Printf("p.Threads : %d", p.Threads)
+
+			//BASELINE GOL LOGIC
 			var wg = sync.WaitGroup{}        //used to make sure all goroutines have done executing before resuming
 			wg.Add(p.Threads)                //add number of threads the wait group needs to wait
 			for i := 0; i < p.Threads; i++ { //for each thread make the worker work??
-				if powOfTwo {
-					go worker(p, c, world, updateWorld, i, workerHeight,  extra, turn, &wg)
-				} else {
-					go worker(p, c, world, updateWorld, i, workerHeight,  extra, turn, &wg)
-				}
+				go worker(p, c, world, updateWorld, i, workerHeight, turn, &wg)
 			}
 
 			wg.Wait() //wait till all goroutines is done (wg == 0)
@@ -238,8 +232,6 @@ func distributor(p Params, c distributorChannels, keyChan <-chan rune) {
 			}
 		}
 	}
-
-	// TODO: Report the final state using FinalTurnCompleteEvent.
 
 	// put FinalTurnComplete into events channel
 	c.events <- FinalTurnComplete{turn, aliveCells}
